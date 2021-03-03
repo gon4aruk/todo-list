4deb8740147a9673d59dfcb4ca6e39775fe8bdc5
gon4aruk <sashagonch27@gmail.com>
2021-03-03 23:13:17 +0200

    npm scripts

--- src/scripts/__tests__/renderTasks.spec.js
+++ src/scripts/__tests__/renderTasks.spec.js
@@ -3,13 +3,11 @@ import { createCheckbox } from "../renderTasks";
 it("shound contains class", () => {
   const checkboxElem = createCheckbox(false, 1);
 
-  expect(checkboxElem.classList.contains('list__item-checkbox')).toEqual(true);
+  expect(checkboxElem.classList.contains("list__item-checkbox")).toEqual(true);
 });
 
-
 it("shound contains data-id", () => {
-    const checkboxElem = createCheckbox(false, 1);
-  
-    expect(checkboxElem.dataset.id).toEqual('1');
-  });
-  
+  const checkboxElem = createCheckbox(false, 1);
+
+  expect(checkboxElem.dataset.id).toEqual("1");
+});
